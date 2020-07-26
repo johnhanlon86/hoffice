4e10f5664762bcb67e324b25f1654bdf17e4d358
johnhanlon86 <johnhanlon86@gmail.com>
2020-07-26 17:53:35 +0100

    Hoffice App V1.0

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+    
+    
+  resources :items
+  
+  root 'static_pages#home'  
+    
+  get '/products' => "items#index"
+
+  get '/about' => 'static_pages#about'
+  
+  get '/help' => 'static_pages#help'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
