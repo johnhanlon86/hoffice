0adecdb40a0492f09c0fa515d6e1e7961b6864df
Simon Monaghan <smono89@gmail.com>
2020-07-30 19:07:47 +0100

    added cart controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     
     
+  get 'cart/index'
   devise_for :users
   resources :items
   
@@ -11,6 +12,8 @@ Rails.application.routes.draw do
   get '/about' => 'static_pages#about'
   
   get '/help' => 'static_pages#help'
+
+  get '/cart', to: 'cart#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
