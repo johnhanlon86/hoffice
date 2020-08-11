192b1d878e2691f241274d9b03505ed9a4d69609
Simon Monaghan <smono89@gmail.com>
2020-08-11 23:50:18 +0100

    heroku test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
   
   config.assets.precompile += ['static_pages.css']
 
