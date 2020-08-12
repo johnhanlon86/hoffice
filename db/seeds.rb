f97b5599d2967b741b74d0e5badf8060d9c96f26
DarylHowe <howedaryl@hotmail.com>
2020-08-12 12:26:42 +0100

    Added 2 items to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 items = Item.create([
-    { title: 'Boss Chair', description: 'Chair for Bosses', price: 250, image_url: 'chair.jpg', category: 'Chairs', brand: 'Hoffice Boss' }
+    { title: 'Boss Chair', description: 'Chair for Bosses', price: 250, image_url: 'chair.jpg', category: 'Chairs', brand: 'Hoffice Boss' },
+    { title: 'Workmans Couch', description: 'The Working Mans Couch', price: 380, image_url: 'WorkmansCouch.jpg', category: 'Chairs', brand: 'Workmans' },
+    { title: 'Walnut Desk', description: 'A walnut desk for when you feel like doing some work!', price: 150, image_url: 'WalnutDesk.jpg', category: 'Desks', brand: 'Workmans' }
 ])
